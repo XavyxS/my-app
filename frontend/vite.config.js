935c934fb1d5654d3c82fadcfede53d967b0bb90
XavyxS <francisco.sandoval23b@udgvirtual.udg.mx>
2024-08-26 20:57:09 -0600

    third commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://localhost:5000',
+      '/api': 'https://my-app-backend-5snm.onrender.com/',
     },
   },
 });
