a909a73a7dcb2f01a50a9b34001242dd723e1504
XavyxS <francisco.sandoval23b@udgvirtual.udg.mx>
2024-08-26 22:28:41 -0600

    six commit

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const cors = require('cors'); // Importar cors
 const app = express();
-// const port = 5000;
+
+// Configuración de CORS para permitir solicitudes desde tu frontend
+app.use(cors({
+  origin: 'https://my-app-qk09.onrender.com', // Permitir solicitudes solo desde tu frontend en producción
+}));
 
 app.use(express.json());
 
@@ -9,5 +14,5 @@ app.get('/api/hello', (req, res) => {
 });
 
 app.listen(process.env.PORT || 5000, () => {
-  console.log('Servidor escuchando en el puerto 3000');
+  console.log('Servidor escuchando en el puerto 5000');
 });
